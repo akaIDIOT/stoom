ba486a3150a4c8a2c2b2d8b2d35c5a84509b246c
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2018-05-10 19:19:16 +0200

    Route intersection of games in a silly way

--- stoom.py
+++ stoom.py
@@ -33,17 +33,31 @@ class Stoom(Sanic):
     def add_routes(self):
         self.add_route(uri='/', handler=self.index)
         self.add_route(uri='/api/steam/player/<steam_id:int>/games', handler=self.games)
+        self.add_route(uri='/api/steam/intersect-games/<steam_id1:int>/<steam_id2:int>', handler=self.intersect_games)
 
     async def index(self, request):
         template = self._env.get_template('stoom/index.html')
         return html(template.render())
 
-    async def games(self, request, steam_id):
+    async def get_games(self, steam_id):
         games = await self.client.get(self.urls.get('games'), params={'key': self._config.steam.api_key,
                                                                       'steamid': steam_id,
                                                                       'format': 'json'})
         games = await games.json()
-        return json(sorted(game['appid'] for game in games['response']['games']))
+        return sorted(game['appid'] for game in games['response']['games'])
+
+    async def games(self, request, steam_id):
+        return json(await self.get_games(steam_id))
+
+    async def get_intersecting_games(self, steam_id, *friends):
+        games = set(await self.get_games(steam_id))
+        for friend in friends:
+            games &= set(await self.get_games(friend))
+
+        return sorted(games)
+
+    async def intersect_games(self, request, steam_id1, steam_id2):
+        return json(await self.get_intersecting_games(steam_id1, steam_id2))
 
 
 if __name__ == '__main__':
